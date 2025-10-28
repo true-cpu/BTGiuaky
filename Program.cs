2e7d1b9ef4ca4714d5ee031c0604111d34babeb0
true-cpu <kiet207092005@gmail.com>
2025-10-28 20:26:51 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string plaintext = "hello";
+
+            int bitSize = 16;
+
+            RSA rsa = new RSA(bitSize);
+
+            Console.WriteLine(" p: " + rsa.p);
+            Console.WriteLine(" q: " + rsa.q);
+            Console.WriteLine(" N: " + rsa.N);
+            Console.WriteLine(" phi: " + rsa.phi);
+            Console.WriteLine(" e: " + rsa.e);
+            Console.WriteLine(" d: " + rsa.d);
+            Console.WriteLine(" PublicKey (e,N): " + rsa.e + ", " + rsa.N);
+            Console.WriteLine(" PrivateKey (d,N): " + rsa.d + ", " + rsa.N);
+
+            Console.WriteLine(" plaintext: " + plaintext);
+
+            BigInteger[] encryptedValues = rsa.Encrypt(plaintext);
+            Console.Write(" Encrypted values: ");
+            foreach (BigInteger encryptedValue in encryptedValues)
+            {
+                Console.Write(encryptedValue + " ");
+            }
+
+            // Giải mã
+            string decryptedText = rsa.Decrypt(encryptedValues);
+            //rsa.Decrypt(encryptedValues);
+            Console.WriteLine("\n Decrypted text: " + decryptedText);
+
+            Console.
